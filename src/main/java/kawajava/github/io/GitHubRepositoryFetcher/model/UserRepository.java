c0a4096652a4139ca6de83e22c84f9f3c2ef32b2
kawajava <kawajava0550@gmail.com>
2025-08-11 13:50:58 +0200

    Implemented service methods

---
+++ src/main/java/kawajava/github/io/GitHubRepositoryFetcher/model/UserRepository.java
@@ -0,0 +1,4 @@
+package kawajava.github.io.GitHubRepositoryFetcher.model;
+
+public record UserRepository(String repositoryName, String ownerLogin) {
+}
--- src/main/java/kawajava/github/io/GitHubRepositoryFetcher/service/GitHubUserService.java
+++ src/main/java/kawajava/github/io/GitHubRepositoryFetcher/service/GitHubUserService.java
@@ -1,7 +1,61 @@
 package kawajava.github.io.GitHubRepositoryFetcher.service;
 
+import kawajava.github.io.GitHubRepositoryFetcher.exception.NotExistingUserException;
+import kawajava.github.io.GitHubRepositoryFetcher.model.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class GitHubUserService {
+
+    private final WebClient webClient;
+
+    public Mono<List<RepositoryWithBranches>> getGitHubUserRepositoriesData(String username) {
+        return dropForks(username)
+                .flatMap(repository -> getRepositories(repository, username))
+                .collectList();
+    }
+
+    private Flux<RepositoryWithBranches> getRepositories(UserRepository repository, String username) {
+        return fetchBranches(username, repository.repositoryName())
+                .map(this::mapToBranch)
+                .collectList()
+                .flatMapMany(branches -> Flux.just(new RepositoryWithBranches(repository.repositoryName(), repository.ownerLogin(), branches)));
+    }
+
+    private Flux<RepositoryDto> fetchRepositories(String username) {
+        return this.webClient.get()
+                .uri("/users/{username}/repos", username)
+                .retrieve()
+                .onStatus(status -> status.value() == 404,
+                        response -> Mono.error(new NotExistingUserException(username)))
+                .bodyToFlux(RepositoryDto.class);
+    }
+
+    private Branch mapToBranch(BranchDto branch) {
+        return new Branch(branch.name(), branch.commit().sha());
+    }
+
+    private UserRepository mapToUserRepository(RepositoryDto repository) {
+        return new UserRepository(repository.name(), repository.owner().login());
+    }
+
+    private Flux<BranchDto> fetchBranches(String username, String repoName) {
+        return this.webClient.get()
+                .uri("/repos/{user}/{repo}/branches", username, repoName)
+                .retrieve()
+                .bodyToFlux(BranchDto.class);
+    }
+
+    private Flux<UserRepository> dropForks(String username) {
+        return fetchRepositories(username)
+                .filter(repository -> !repository.isFork())
+                .map(this::mapToUserRepository);
+    }
 }
